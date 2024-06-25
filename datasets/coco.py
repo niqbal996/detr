02d82ca2485775c057da311b65265e15234d3e8e
Naeem Iqbal <naeemiqbal996@gmail.com>
2024-06-25 16:46:44 +0200

    Added custom dataloader and model classes

--- datasets/coco.py
+++ datasets/coco.py
@@ -145,14 +145,20 @@ def make_coco_transforms(image_set):
 
 
 def build(image_set, args):
-    root = Path(args.coco_path)
-    assert root.exists(), f'provided COCO path {root} does not exist'
-    mode = 'instances'
-    PATHS = {
-        "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
-    }
+    # root = Path(args.coco_path)
+    # assert root.exists(), f'provided COCO path {root} does not exist'
+    # mode = 'instances'
+    # PATHS = {
+    #     "train": (root / "train2017", root / "annotations" / f'{mode}_train2017.json'),
+    #     "val": (root / "val2017", root / "annotations" / f'{mode}_val2017.json'),
+    # }
 
+    # img_folder, ann_file = PATHS[image_set]
+    PATHS = {
+        # "train":("/netscratch/naeem/sugarbeet_syn_v6/images", "/netscratch/naeem/sugarbeet_syn_v6/coco_annotations/instances_train.json"),
+        "train":("/netscratch/naeem/phenobench/train/", "/netscratch/naeem/phenobench/coco_annotations/coco_plants_panoptic_train.json"),
+        "val":("/netscratch/naeem/phenobench/val/", "/netscratch/naeem/phenobench/coco_annotations/coco_plants_panoptic_val.json"),
+    }
     img_folder, ann_file = PATHS[image_set]
     dataset = CocoDetection(img_folder, ann_file, transforms=make_coco_transforms(image_set), return_masks=args.masks)
     return dataset
